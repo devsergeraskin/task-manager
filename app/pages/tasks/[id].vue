b42da24e718476ec7d55d31d5bc933529f653aed
serge <devsergeraskin@gmail.com>
2026-01-06 17:16:24 +0200

    init

---
+++ app/pages/tasks/[id].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>Task detail page</div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ app/pages/tasks/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-gray-100 min-h-screen">
+    <div class="container mx-auto px-4 py-8 text-secondary">
+      <h1 class="text-3xl font-bold text-center mb-8">Localazy assignment</h1>
+      <ol class="list-decimal list-inside">
+        <li>Read the assigment in the root of the project</li>
+        <li>Fullfill the three tasks described in the assigment file</li>
+        <li>
+          Create as many components you need to keep the code clean and readable
+        </li>
+        <li>
+          Use your own composables or VueUse composables to help you with the
+          application logic
+        </li>
+        <li>
+          Make sure to fulfill all the requirements of each task, but don't
+          hesitate to enhance the application with your own ideas
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
